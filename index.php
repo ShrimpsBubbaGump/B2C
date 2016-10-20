d0e22d232ee86bd2c6d1588e61c3f16bca37b241
ShrimpsBubbaGump <flahautfrancois@gmail.com>
2016-10-20 19:28:17 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+   <script type="text/javascript" src="js/menu.js"></script>
+    <title>Accueil</title>
+</head>
+<body>
+
+<?php include("nav.php");?>
+<?php include("carousel.php");?>
+<article>
+
+    <h1> Croisières en promo!!!</h1>
+        <table>
+            <tr>
+                <td>
+            <img src="img/actuCroisiere1.jpg">
+                    <p>profiter de cete offre de dernière minute!!!</p>
+                </td>
+                <td>
+                    <img src="img/actuCroisiere3.jpg">
+                    <p>sjsjdkljdsjfiosd</p>
+                </td>
+                <td>
+                <img src="img/actuCroisiere4.jpg">
+                <p>edjsjlksqldslqm.</p>
+                </td>
+
+            </tr>
+
+        </table>
+
+
+
+</article>
+</body>
+</html>
